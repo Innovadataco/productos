0d48c967c97b6a47958c64d1cb715fc2f63f02a2
Innovadataco <gerencia@innovadataco.com>
2026-09-14 12:23:45 -0500

    008: reinicio completo del proyecto PGN estudio

--- 008-2026-PGN-SIMULACRO/lib/static.ts
+++
@@ -1,9 +0,0 @@
-import { TOPICS } from './types'
-
-export function generateTopicParams() {
-  const keys = new Set<string>()
-  Object.values(TOPICS)
-    .flat()
-    .forEach((t) => keys.add(t.key))
-  return Array.from(keys).map((tema) => ({ tema }))
-}
